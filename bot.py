2ac51ddef1dfee387c8e96a1b0f11f9584f99c48
Lait96 <qqqqz96@mail.ru>
2021-11-04 11:26:45 +0300

    bot on the twitchio library

--- bot.py
+++ bot.py
@@ -1,35 +1,43 @@
+from twitchio.ext import commands
 import config
 import utils
-import socket
-import re
-import time
+
+bot = commands.Bot(
+    token=config.PASS,
+    client_id=config.SECRET,
+    nick=config.NICK,
+    prefix='!',
+    initial_channels=config.CHAN
+)
+
+in_duel = False
+duel_login = ''
 
 
-def main():
-    s = socket.socket()
-    s.connect((config.HOST, config.PORT))
-    s.send("PASS {}\r\n".format(config.PASS).encode('UTF-8'))
-    s.send("NICK {}\r\n".format(config.NICK).encode('UTF-8'))
-    s.send("JOIN {}\r\n".format(config.CHAN).encode('UTF-8'))
+@bot.command(name='дуэль')
+async def duel1(ctx):
+    global in_duel, duel_login
+    login = utils.get_login(ctx)
+    if duel_login == login:
+        await ctx.send(f"@{login} вызов уже брошен! Ожидайте оппонента!")
+    elif in_duel:
+        txt, log = utils.get_random_duel([login, duel_login])
+        in_duel = False
+        duel_login = ''
+        print(log)
+        await ctx.send(txt)
+    else:
+        duel_login = login
+        in_duel = True
+        await ctx.send(f"@{login} объявил дуэль на орехомётах. Напиши !дуэль чтобы принять его вызов")
 
-    chat_message = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
-    # utils.mess(s, 'Драсте')
-    count = 0
-    while True:
-        count += 1
-        print(count)
-        response = s.recv(4096).decode('UTF-8')
-        if response == 'PING :tmi.twitch.tv\r\n':
-            print(response, 'in if')
-            s.send('PONG :tmi.twitch.tv\r\n'.encode('UTF-8'))
-        else:
-            username = re.search(r"\w+", response).group(0)
-            message = chat_message.sub("", response)
-            print(username, response)
-            if message.strip() == '!time':
-                utils.mess(s, f"@{username} it's time to stop!")
-        time.sleep(1)
+
+@bot.command(name='кусь')
+async def bite(ctx):
+    txt, log = utils.random_bite(ctx)
+    print(log)
+    await ctx.send(txt)
 
 
 if __name__ == "__main__":
-    main()
+    bot.run()
--- utils.py
+++ utils.py
@@ -1,5 +1,68 @@
+import requests
 import config
+from random import shuffle, choice
 
 
-def mess(sock, message):
-    sock.send("PRIVMSG #{} :{}\r\n".format(config.CHAN, message).encode('UTF-8'))
+def get_random_users(channel, login):
+    users = requests.get(f'https://tmi.twitch.tv/group/user/{channel}/chatters')
+    print(users)
+    users = users.json()
+
+    def flatten(x):
+        result = []
+        for el in x:
+            if hasattr(el, "__iter__") and not isinstance(el, str):
+                result.extend(flatten(el))
+            else:
+                result.append(el)
+        return result
+
+    lis = []
+    lis.extend(users['chatters'].values())
+    users_list = list(flatten(lis))
+    shuffle(users_list)
+    while True:
+        if users_list[0] == login:
+            shuffle(users_list)
+        else:
+            break
+    return users_list[0]
+
+
+def get_login(ctx):
+    info = dict(item.split("=") for item in ctx.message.raw_data.split(";"))
+    print(info)
+    return str(info['display-name']).lower()
+
+
+def random_bite(ctx):
+    login = get_login(ctx)
+    while True:
+        random_login = get_random_users(config.CHAN[0], login)
+        if random_login != login:
+            break
+    speak = [f'@{login} кусает за жепку {random_login}',
+             f'@{login} внезапно кусает @{random_login} за ухо',
+             f'@{login} делает нежный кусь @{random_login}',
+             f'@{login} прыгает на @{random_login} и кусает за шею',
+             f'@{login} совершает мега кусь за локоть @{random_login}',
+             f'@{login} совершает множественный кусь @{random_login}']
+    log = f'Внимание хозяин!\n{login} укусил {random_login}'
+    txt = choice(speak)
+    return txt, log
+
+
+def get_random_duel(lst):
+    shuffle(lst)
+    win, lus = lst
+    speak = [f'@{win} выхватывает свой орехомёт и стреляет в @{lus}. Есть, точное попадание! @{lus} повержен',
+             f'{win} безжалостно расстрелял орехами {lus}',
+             f'@{lus} промахивается. Тем временем @{win} делает точный выстрел, @{lus} повержен',
+             f'@{win} промахивается, чем же ответит @{lus}? Тоже промах? Что же, в этой схватке нет победителей',
+             f'Оба дуэлянта,@{win} и @{lus}, выстрелили одновременно и поразили друг друга. В этой схватке проиграли оба участника!']
+    txt = choice(speak)
+    if 'победителей' in txt or 'проиграли' in txt:
+        log = f'Внимание хозяин!\n{win} и {lus} стреляются\nУ них ничья!'
+    else:
+        log = f'Внимание хозяин!\n{win} застрелил {lus}'
+    return txt, log
